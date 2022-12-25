d588a37b3c77d5e8d109e275559c64e2f7a7aae4
David Cuesta Martín <david-3-6@users.noreply.github.com>
2022-12-25 15:30:08 +0100

    Practica 3 y 4 de TALF

---
+++ Practica 4/vectores.m
@@ -0,0 +1,8 @@
+function vectores ()
+i=0;
+while (0<=0)
+disp(['(' num2str(godeldecoding(i)) ')' ]);
+i=i+1;
+end
+
+end
---
+++ Practica 4/whiles.m
@@ -0,0 +1,8 @@
+function vectores ()
+i=0;
+while (0<=0)
+disp([N2WHILE(i)]);
+i=i+1;
+end
+
+end
